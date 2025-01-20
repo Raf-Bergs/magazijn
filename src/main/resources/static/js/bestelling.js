315ba38ca79e55efde2be35df20c1fde037d403a
Rene <rene.talukder@vdabcampus.be>
2025-01-20 13:29:15 +0100

    bestelling.html voorbeeld aangepast. .checked in style.css bijgevoegd. bestelling.js order voltooid button

--- src/main/resources/static/js/bestelling.js
+++ src/main/resources/static/js/bestelling.js
@@ -1,2 +1,18 @@
 "use strict"
 import {verberg, toon, byId, verwijderChildElementenVan, setText} from "./util.js";
+
+let bestellijnCheckboxen = document.querySelectorAll('input[type="checkbox"]');
+
+
+//Eventlistener voor order voltooid button.
+//Button wordt geactiveerd wanneer alle checkbox gechecked zijn.
+document.addEventListener("change", () => {
+        for (let i = 0; i < bestellijnCheckboxen.length; i++) {
+            if (!bestellijnCheckboxen[i].checked) {
+                document.getElementById("orderVoltooidBtn").disabled = true;
+                return
+            }
+        }
+        document.getElementById("orderVoltooidBtn").disabled = false;
+    }
+)
