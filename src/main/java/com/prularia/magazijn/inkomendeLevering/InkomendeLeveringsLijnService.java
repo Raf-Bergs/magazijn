a7d301e6a08ceef39ef3233dc0e6e97a0313cc23
AxelBO <axel.boonen.mail@gmail.com>
2025-01-23 14:05:55 +0100

    service update

--- src/main/java/com/prularia/magazijn/inkomendeLevering/InkomendeLeveringsLijnService.java
+++ src/main/java/com/prularia/magazijn/inkomendeLevering/InkomendeLeveringsLijnService.java
@@ -1,4 +1,17 @@
 package com.prularia.magazijn.inkomendeLevering;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class InkomendeLeveringsLijnService {
+
+    private final InkomendeLeveringsLijnRepository inkomendeLeveringsLijnRepository;
+
+    public InkomendeLeveringsLijnService(InkomendeLeveringsLijnRepository inkomendeLeveringsLijnRepository) {
+        this.inkomendeLeveringsLijnRepository = inkomendeLeveringsLijnRepository;
+    }
+
+    public int createInkomendeLeveringsLijn(InkomendeLeveringsLijn inkomendeLeverings) {
+        return inkomendeLeveringsLijnRepository.createInkomendeLeveringsLijn(inkomendeLeverings);
+    }
 }
