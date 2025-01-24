1df551285c5d1581bc80f2031ed27e83a3c5fc87
SebastiaanVO <s.van.overschee@gmail.com>
2025-01-24 10:36:04 +0100

    Klein testje

--- src/main/java/com/prularia/magazijn/inkomendeLevering/InkomendeLeveringController.java
+++ src/main/java/com/prularia/magazijn/inkomendeLevering/InkomendeLeveringController.java
@@ -1,7 +1,11 @@
 package com.prularia.magazijn.inkomendeLevering;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("leveringen")
@@ -10,5 +14,18 @@ public class InkomendeLeveringController {
 
     public InkomendeLeveringController(InkomendeLeveringService inkomendeLeveringService) {
         this.inkomendeLeveringService = inkomendeLeveringService;
+    }
+
+    @GetMapping
+    public void leveringen() {
+        System.out.println(inkomendeLeveringService.vindMagazijnPlaatsen(maakInkomendeLeveringslijnen()));
+    }
+
+    private List<InkomendeLeveringslijnDTO> maakInkomendeLeveringslijnen() {
+        var lijnen = new ArrayList<InkomendeLeveringslijnDTO>();
+        lijnen.add(new InkomendeLeveringslijnDTO(1, "test1", "", 5, 0, null));
+        lijnen.add(new InkomendeLeveringslijnDTO(2, "test2", "", 4, 0, null));
+        lijnen.add(new InkomendeLeveringslijnDTO(3, "test3", "", 10, 0, null));
+        return lijnen;
     }
 }
